d103307d99c2327fedf0aa421997f9440d73b109
Oli Hensel <git@codeherd.de>
2022-09-21 12:35:03 +0200

    fix: add missing intent

--- discord/lib/discordBotManager.js
+++ discord/lib/discordBotManager.js
@@ -15,7 +15,8 @@ var getBot = function (configNode) {
           Intents.FLAGS.GUILD_MESSAGES,
           Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
           Intents.FLAGS.DIRECT_MESSAGES,
-          Intents.FLAGS.DIRECT_MESSAGE_REACTIONS
+          Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+          Intents.FLAGS.GUILD_VOICE_STATES
         ],
         partials: [
           "CHANNEL",
