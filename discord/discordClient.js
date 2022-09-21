c3e1abf6be61d5c2ba5b47b106cafb794bd56cf8
Oli Hensel <git@codeherd.de>
2022-09-21 11:38:18 +0200

    feat: pass voice through client

--- discord/discordClient.js
+++ discord/discordClient.js
@@ -1,26 +1,31 @@
-module.exports = function(RED) {
-    var discordBotManager = require('./lib/discordBotManager.js');
+const voice = require("@discordjs/voice");
+module.exports = function (RED) {
+  var discordBotManager = require("./lib/discordBotManager.js");
 
-    function discordClient(config) {
-        RED.nodes.createNode(this, config);
-        var configNode = RED.nodes.getNode(config.token);
-        var node = this;
-        discordBotManager.getBot(configNode).then(function(bot){
-            node.on('input', function(msg) {
-                msg.discord = bot;
-                node.send(msg);
-            });
-            node.on('close', function() {
-                discordBotManager.closeBot(bot);
-            });            
-        }).catch(err => {
-            console.log(err);
-            node.status({
-                fill: "red",
-                shape: "dot",
-                text: err
-            });
+  function discordClient(config) {
+    RED.nodes.createNode(this, config);
+    var configNode = RED.nodes.getNode(config.token);
+    var node = this;
+    discordBotManager
+      .getBot(configNode)
+      .then(function (bot) {
+        node.on("input", function (msg) {
+          msg.discord = bot;
+          msg.voice = voice;
+          node.send(msg);
         });
-    }
-    RED.nodes.registerType("discordClient", discordClient);
+        node.on("close", function () {
+          discordBotManager.closeBot(bot);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        node.status({
+          fill: "red",
+          shape: "dot",
+          text: err,
+        });
+      });
+  }
+  RED.nodes.registerType("discordClient", discordClient);
 };
